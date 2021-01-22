ed5d6ffcb172a6f015ce3521e3e535715c841870
Junior Tavares <joaldo.junior@ucsal.edu.br>
2021-01-22 14:25:23 -0300

    Get /products

---
+++ backend/src/main/java/com/dsdeliver/dsdeliver/services/ProductService.java
@@ -0,0 +1,41 @@
+package com.dsdeliver.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dsdeliver.dsdeliver.dto.ProductDTO;
+import com.dsdeliver.dsdeliver.entities.Product;
+import com.dsdeliver.dsdeliver.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<ProductDTO> findAll(){
+		List<Product> list = repository.findAllByOrderByNameAsc();
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
