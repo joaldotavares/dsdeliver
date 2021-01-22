2e29ede45a9028135f266e61b92b93827bc2798c
Junior Tavares <joaldo.junior@ucsal.edu.br>
2021-01-22 14:52:32 -0300

    Get /orders

---
+++ backend/src/main/java/com/dsdeliver/dsdeliver/services/OrderService.java
@@ -0,0 +1,36 @@
+package com.dsdeliver.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dsdeliver.dsdeliver.dto.OrderDTO;
+import com.dsdeliver.dsdeliver.entities.Order;
+import com.dsdeliver.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
