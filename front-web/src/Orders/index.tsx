09abce1755df11919069a596df8de8da5f85ed27
Junior Tavares <joaldo.junior@ucsal.edu.br>
2021-02-03 23:38:10 -0300

    Routes with React Routers

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import {ReactComponent as Logo} from './logo.svg';
+
+function Orders(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default Orders;
