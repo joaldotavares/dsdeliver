7f2d89b5fcf14a6f1a0d1d849bcb31c24a2097d0
Junior Tavares <joaldo.junior@ucsal.edu.br>
2021-03-03 16:03:24 -0300

    Finish frontend web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
 const API_URL = 'http://localhost:8080';
-const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts(){
     return axios(`${API_URL}/products`)
