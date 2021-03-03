2e1990eaeaab1acd2f5d33c4b72f535bc2afca3e
Junior Tavares <joaldo.junior@ucsal.edu.br>
2021-03-03 16:37:22 -0300

    Add vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
-const API_URL = 'https://joaldo-dsdeliver.herokuapp.com/';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export function fetchProducts(){
     return axios(`${API_URL}/products`)
